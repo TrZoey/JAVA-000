c3b85335e337cfaf5e17dc17ae6135e41c285119
徐世伟 <384128657@qq.com>
2020-10-20 16:36:19 +0800

    作业一：自定义类加载器

---
+++ Week_01/com/prik/work01/MyClassLoader.java
@@ -0,0 +1,54 @@
+package com.prik.work01;
+
+import java.io.*;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @description: 自定义类加载器
+ * @author: Xu Shiwei
+ * @create: 2020-10-20
+ **/
+public class MyClassLoader extends ClassLoader {
+
+    /**
+     * 重写 findClass 方法
+     * @param name
+     * @return
+     * @throws ClassNotFoundException
+     */
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+
+        // 1. 找到文件，转换为字节流
+        File file = new File(name);
+        FileInputStream inputStream;
+        try {
+            inputStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            throw new ClassNotFoundException(name);
+        }
+
+        // 2. 对字节进行处理
+        int value;
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try {
+            while ((value = inputStream.read()) != -1) {
+                outputStream.write(255 - value);
+            }
+        } catch (IOException e) {
+            throw new ClassNotFoundException(name, e);
+        }
+
+        // 3. 调用方法，定义一个类
+        byte[] bytes = outputStream.toByteArray();
+        return defineClass("Hello", bytes, 0, bytes.length);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException,
+            InstantiationException, NoSuchMethodException, InvocationTargetException {
+        MyClassLoader myClassLoader = new MyClassLoader();
+        Class<?> hello = myClassLoader.loadClass("Week_01/Hello.xlass");
+        Object instance = hello.newInstance();
+        hello.getMethod("hello").invoke(instance);
+    }
+}
--- out/production/JAVA-000/README.md
+++ out/production/JAVA-000/README.md
@@ -3,7 +3,7 @@
 ### 01. 
 
 题目：自定义一个 Classloader，加载一个 Hello.xlass 文件，执行 hello 方法，此文件内容是一个 Hello.class 文件所有字节（x=255-x）处理后的文件。文件群里提供。
-解答：[自定义类加载器](com/prik/work01/MyClassLoader.java)
+解答：
 
 ### 02. 
 
